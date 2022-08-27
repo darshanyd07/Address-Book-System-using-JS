cd9e5ba5c8122082d02f3b0599cc83bfc2e6f920
champ09D <darshanmhatre709@gmail.com>
2022-08-27 12:57:53 +0530

    Added : Ability_To_Ensure_Valid_Contacts Add

--- Js/Address_Book.js
+++ Js/Address_Book.js
@@ -1,7 +1,8 @@
-const nameRegex;
-const addressRegex ;
-const cityStateRegex;
-const zipRegex;
-const phoneNumberRegex;
-const emailRegex;
- 
+const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+const addressRegex = RegExp("^[a-zA-z0-9#,]{4,}$");
+const cityStateRegex = RegExp("^[a-zA-z]{4,}$");
+const zipRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+const phoneNumberRegex = RegExp("^[0-9]{2}[-]{1}[0-9]{10}$");
+const emailRegex = RegExp(
+  "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$"
+);
